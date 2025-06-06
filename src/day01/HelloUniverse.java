32a20455b2f472e594a7ae9b123c97bfdc2e41c2
Sherab <tenpadhma@gmail.com>
2025-06-06 13:42:17 -0700

    HelloUniverse updated again

--- src/day01/HelloUniverse.java
+++ src/day01/HelloUniverse.java
@@ -5,5 +5,6 @@ public class HelloUniverse {
     public static void main(String[] args) {
 
         System.out.println("Hello Universe");
+        System.out.println("I hope my Java journey is fun!");
     }
 }
